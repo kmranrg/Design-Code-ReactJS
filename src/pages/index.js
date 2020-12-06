db7d5db451f252e9b237b9b9cb9fb22ce0e28640
kmranrg <kmranrg@gmail.com>
2020-12-07 01:08:17 +0530

    removed github workflows

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ const IndexPage = () => (
     <div className="Hero">
       <div className="HeroGroup">
         <h1>Learn to <br /> design and code React apps</h1>
-        <p>Complete courses about the best tools and design systems. Prototype and buld apps with React and Swift.</p>
+        <p>Complete courses about the best tools and design systems. Prototype and buld apps with React & Swift.</p>
         <Link to="/page-2/">Watch the video</Link>
         <div className="Logos">
           <img src={require('../images/logo-sketch.png')} width="50" />
